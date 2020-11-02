669e4e0a9f4dbb2daeda74f5781e5c21d66abbe5
nikodemicek <56070150+nikodemicek@users.noreply.github.com>
2020-11-02 21:44:41 +0100

    Links fixing
    Added column for linked content and removed links from clean text - no https shows up in wordcloud any longer

--- preprocessing.py
+++ preprocessing.py
@@ -10,7 +10,9 @@ import os
 
 def load_data(folder):
     #get current directory and input the folder to analyze
-    directory_path = os.getcwd() + '/' + folder + '/'
+    data_url = f"CBS - Copenhagen Business School/Kick-Ass Master Thesis - General/Data/{folder}/Raw Data/"
+    directory_path = os.getcwd() + '/../../../' + data_url 
+    print(directory_path)
     event_df = pd.DataFrame()
     #append all csv files in the folder to each other
     for file_name in glob.glob(directory_path + '*.csv'):
@@ -33,25 +35,54 @@ def load_data(folder):
         "Twitter Followers":"Followers",
         "Twitter Following":"Following"},inplace = True)
     
-    return event_df
+    return event_df.iloc[0:1000,:]
 
 
 def lemma(text):
     lemmatizer = WordNetLemmatizer()
     return [lemmatizer.lemmatize(word) for word in text]
 
+def tokenize(text):
+    return text.apply(lambda x: word_tokenize(str(x)))  
+
 def remove_stopwords(text, stop_words):
     stopped = [word for word in text if (word not in stop_words)]
     return [word for word in stopped if (len(word) > 3)]
 
-def preprocess_text(text):
+def clean_text(text):
+    #remove links
+    text = text.apply(lambda x: re.sub('[http|https]+://[\w\S(\.|:|/)]+',' ', str(x)))
     #remove the tagged account names
     text = text.apply(lambda x: re.sub('\@(.*?:)',' ', str(x)))
-    #remove everything that is not a letter
+
+    #remove everything that is not a letter and set to lowercase
     text = text.apply(lambda x: re.sub('[^a-zA-Z]', ' ', str(x)).lower())
-    text = text.apply(lambda x: word_tokenize(str(x)))
-    text = text.apply(lemma)
+
+    #clean \n
+
+    """
+    TO DO:
+    Clean links as well (https shows in the wordcloud)
+    Progress bar (each 10% or something)
+    - bigrams/trigrams to see words in connection with stopwords like NOT etc. and word that belong together (get support from theory)
+    - implement and compare with the "preprocess" package
+    - feature of hashtags/mentions (number of hashtags/mentions)
+    - feature length of tweets (in words and characters)
+    """
     return text
+
+def create_feature_columns(event_df):
+
+    event_df["Hashtags"] = event_df["Tweet Raw"].apply(lambda x: re.findall('#(\w+)',str(x)))
+    event_df["Mentions"] = event_df["Tweet Raw"].apply(lambda x: re.findall('@(\w+)',str(x)))
+    event_df["Linked Content"] = event_df["Tweet Raw"].apply(lambda x: re.findall('[http|https]+://[\w\S(\.|:|/)]+',str(x)))
+    event_df["Retweet"] = event_df["Tweet Raw"].apply(lambda x: re.search('RT',str(x))!= None)
+    event_df["Quote Tweet"] = event_df["Tweet Raw"].apply(lambda x: re.search('QT',str(x)) != None)
+    return event_df
+    """
+    Include RT column (boolean)
+    include linked content columns
+    """
 
 
 def get_processed_data(folder):
@@ -61,11 +92,19 @@ def get_processed_data(folder):
     print('preprocess tweets...')
     #select only the ones that have reached audience higher than x 
     event_df = event_df[event_df['Reach'] > 0]
-    event_df["Hit_Sentence_Clean"] = preprocess_text(event_df["Hit Sentence"])
+
+    event_df = create_feature_columns(event_df)
+
+    event_df['Tweet Clean'] = clean_text(event_df["Tweet Raw"])
+
+    event_df["Tweet Clean Tokens"] = tokenize(event_df["Tweet Clean"])
+    event_df["Tweet Clean Tokens"] = event_df["Tweet Clean Tokens"].apply(lemma)
+
     print('removing unwanted words...')
     stop_words = stopwords.words('english')
-    extra_stopwords = ['twitter','http'] 
+    extra_stopwords = ['twitter','http','https'] 
     stop_words = list(stop_words) + extra_stopwords
-    event_df["Hit_Sentence_Clean"] = event_df["Hit_Sentence_Clean"].apply(lambda x: remove_stopwords(x, stop_words))
+    event_df["Tweet Clean Tokens"] = event_df["Tweet Clean Tokens"].apply(lambda x: remove_stopwords(x, stop_words))
+
     print('DONE! Processed ',event_df.shape[0],' tweets ready for analysis!' )
     return event_df
