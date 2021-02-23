03017d53944f3f3607b18e4b74d707411a150f6b
nikodemicek <56070150+nikodemicek@users.noreply.github.com>
2021-02-23 17:26:56 +0100

    cand merging

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,10 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import glob
 import os 
-import preprocessor as p
+import numpy as np
+import preprocessor
+from difflib import SequenceMatcher as sm
+from tqdm import tqdm
 
 
 def load_data(folder):
@@ -14,11 +17,11 @@ def load_data(folder):
 	#get current directory and input the folder to analyze
 	data_url = f"CBS - Copenhagen Business School/Kick-Ass Master Thesis - General/Data/{folder}/Raw Data/"
 	directory_path = os.getcwd() + '/../../../' + data_url 
-	print(directory_path)
+	#print(directory_path)
 	event_df = pd.DataFrame()
 	#append all csv files in the folder to each other
-	for file_name in glob.glob(directory_path + '*.csv'):
-	    print('Reading file: ' + file_name)
+	for file_name in tqdm(glob.glob(directory_path + '*.csv')):
+	    #print('Reading file: ' + file_name)
 	    sheet = pd.read_csv(file_name, sep='\t', encoding = 'utf_16')
 	    event_df = pd.concat([event_df,sheet],axis=0)
 	#choose only relevant columns to analyze
@@ -39,7 +42,7 @@ def load_data(folder):
 
 	print('loaded ', event_df.shape[0], ' tweets.')
 
-	return event_df#.iloc[0:1000,:]
+	return event_df#.iloc[0:10000,:]
 
 
 def lemma(text):
@@ -84,8 +87,8 @@ def create_feature_columns(event_df):
     event_df["Mentions"] = event_df["Tweet Raw"].apply(lambda x: re.findall('@(\w+)',str(x)))
     event_df["Linked Content"] = event_df["Tweet Raw"].apply(lambda x: re.findall('[http|https]+://[\w\S(\.|:|/)]+',str(x)))
     #return True if the Tweet Raw text contains RT and QT
-    event_df["Retweet"] = event_df["Tweet Raw"].apply(lambda x: re.search('RT',str(x))!= None) #maybe need to improve so it looks only at the start of the string
-    event_df["Quote Tweet"] = event_df["Tweet Raw"].apply(lambda x: re.search('QT',str(x)) != None) #maybe need to improve so it looks only at the start of the string
+    event_df["Retweet"] = event_df["Tweet Raw"].astype(str).str.startswith("RT") #maybe need to improve so it looks only at the start of the string
+    event_df["Quote Tweet"] = event_df["Tweet Raw"].astype(str).str.startswith("QT") #maybe need to improve so it looks only at the start of the string
     return event_df
     """
     Include RT column (boolean)
@@ -101,16 +104,79 @@ def get_processed_data(event_df):
 
     event_df = create_feature_columns(event_df)
 
-    event_df['Tweet Clean'] = event_df["Tweet Raw"].apply(lambda x: p.clean(x).lower())  
+    event_df['Tweet Clean'] = event_df["Tweet Raw"].apply(lambda x: preprocessor.clean(x).lower())  
 
     event_df["Tweet Clean Tokens"] = tokenize(event_df["Tweet Clean"])
     event_df["Tweet Clean Tokens"] = event_df["Tweet Clean Tokens"].apply(lemma)
 
     print('removing unwanted words...')
     stop_words = stopwords.words('english')
-    extra_stopwords = ['twitter','http','https','daniel','trilling'] 
+    extra_stopwords = ['twitter','http','https','daniel','trilling','guardian','bbcnews'] 
     stop_words = list(stop_words) + extra_stopwords
     event_df["Tweet Clean Tokens"] = event_df["Tweet Clean Tokens"].apply(lambda x: remove_stopwords(x, stop_words))
 
     print('DONE! Processed ',event_df.shape[0],' tweets ready for analysis!' )
     return event_df
+
+
+def remove_duplicate_tweets(event_df):
+	event_df.reset_index(drop=True, inplace=True)
+
+	print('removing retweets and quote tweets...')
+	event_df = event_df[event_df['Quote Tweet'] != True]
+	event_df = event_df[event_df['Retweet'] != True]
+
+
+	print('removing duplicates...')
+	event_df['tokens_to_str'] = event_df["Tweet Clean Tokens"].apply(lambda x: ' '.join(x))
+	event_df.drop_duplicates(subset="tokens_to_str", inplace=True)  
+	event_df.drop('tokens_to_str',axis=1,inplace=True)  
+	return event_df
+
+def fuzzy_duplicate_removal(event_df, similarity = 0.9):
+	def find_matches(tweet_to_match):
+	    rows = event_df['Tweet Clean'].values.tolist() #convert the df column to list
+
+	    if (rows.index(tweet_to_match)+1) % 100 ==0:
+	    	print(f'Currently on {rows.index(tweet_to_match)+1}th tweet...')
+
+	    del rows[:rows.index(tweet_to_match)] #remove the previous rows as they have already been matched 
+
+	    #perform similarity calculation using difflib and mark the similarities higher than threshold as duplicates
+	    matches = [sm(None, tweet_to_match, row).ratio() for row in rows]
+	    are_duplicates = [match>similarity for match in matches]
+	    
+	    return are_duplicates[1:] # return list of booleans excluding itself (index 0)
+
+	clean_tweets_df = event_df['Tweet Clean'].to_frame() # so we can use .applymap method we only put the tweet text column (cleaned) into dataframe 
+	
+
+	fn_find_matches = lambda x: find_matches(x)
+	clean_tweets_df['Duplicate']=clean_tweets_df.applymap(fn_find_matches)
+
+	#instantiate the list where we append the locations of duplicate rows
+	dup_locs = {}
+
+	for index, row in clean_tweets_df.iterrows():
+	    # the index of duplicate in the df is 1 + index + position in list of duplicates:
+	    # + index since we start counting in the row in which we are checking
+	    # + 1 since we exclude the similarity of the tweets with itself
+	    # + np.where(clean_tweets_df['Duplicate'].iloc[index])[0] using numpy to get locations of True labels
+	    dup_locs[index] = []
+	    dup_locs[index].extend(index + 1 + np.where(clean_tweets_df['Duplicate'].iloc[index])[0])
+	    
+	    if len(dup_locs[index]) == 0:
+	        del dup_locs[index]
+	    #if True in clean_tweets_df['Duplicate'].iloc[index]:
+	        #duplicated_rows.append(index)
+
+	#print indexes of the fuzzily duplicate rows 
+	print(f'The rows that are more than {similarity*100}% similar are: {dup_locs}')
+
+	# first list of lists of dup_locs dictionary values is converted into a list, and then into a descending sorted set to preserve only unique values
+	indexes_to_remove = sorted(set(list(pd.core.common.flatten(list(dup_locs.values())))),reverse=True)
+	print(f'Removing {len(indexes_to_remove)} fuzzy duplicates')
+
+	event_df.drop(indexes_to_remove,inplace=True)
+
+	return event_df
